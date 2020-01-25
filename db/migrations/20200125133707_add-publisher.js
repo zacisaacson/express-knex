b7d3d356077a341cbab09bce70341eed3fed99e8
Zac Isaacson <isaacson.zm@gmail.com>
2020-01-25 15:41:01 -0700

    Create paper and footnotes migrations

---
+++ db/migrations/20200125133707_add-publisher.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex) {
+  return Promise.all([
+    knex.schema.table('papers',
+function(table) {
+      table.string('publisher');
+    })
+  ]);
+};
+
+exports.down = function(knex) {
+  return Promise.all([
+    knex.schema.table('papers',
+function(table) {
+      table.dropColumn('publisher');
+    })
+  ]);
+};
