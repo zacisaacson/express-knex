6f7fe14f8f20bdb8548258510c0482bb4bba5d00
Zac Isaacson <isaacson.zm@gmail.com>
2020-01-25 15:41:17 -0700

    Add seed file

---
+++ db/seeds/dev/papers.js
@@ -0,0 +1,22 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('footnotes').del()
+    .then(() => knex('papers').del())
+    .then(() => {
+      return Promise.all([
+        knex('papers').insert({
+          title: 'Fooo', author: 'Bob', publisher: 'Minnesota'
+        }, 'id')
+        .then(paper => {
+          return knex('footnotes').insert([
+            { note: 'Lorem', paper_id: paper[0] },
+            { note: 'Dolor', paper_id: paper[0] }
+          ])
+        })
+        .then(() => console.log('Seeding complete!'))
+        .catch(error => console.log(`Error seeding data: ${error}`))
+      ])
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
+};
